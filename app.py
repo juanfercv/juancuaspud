a0e6b1588cdbe08525da188875fbb98d81f75b4b
juanfercv <ferchocodecv2407@gmail.com>
2025-11-06 22:58:46 -0500

    funcionando cambios2 app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola Mundo desde Flask con CI/CD Automatizado! 🚀✅"
-
-@app.route('/health')
-def health():
-    return {"status": "healthy", "service": "Flask App"}
+    return "¡Hola Mundo desde Flask con Traefik! 🚀"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
